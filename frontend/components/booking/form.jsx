781c52a5ca352fdf4f269d10bf88765ee2b76367
Nathan Matteson <nwmatteson@gmail.com>
2016-03-04 11:07:03 -0800

    booking trust banner

--- frontend/components/booking/form.jsx
+++ frontend/components/booking/form.jsx
@@ -26,8 +26,13 @@ module.exports = React.createClass({
   },
 
   render: function () {
+    var path = this.props.location.pathname;
+    var trust1 = " Free hugs if something goes wrong.";
+    var trust2 = " All Taskers have been to the vet.";
+    var bannerContent = path.indexOf("details") !== -1 ? trust1 : trust2;
     return (
       <div className="main-container">
+        <div className="trust-banner"><strong>Trust & Safety Guarantee: </strong>{bannerContent}</div>
         {this.props.children}
       </div>
     );
