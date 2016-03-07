7e3c81a0a3e4338c4f5dffa00c9fc04db8b51b0e
Nathan Matteson <nwmatteson@gmail.com>
2016-03-06 21:36:43 -0800

    style new user and session pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
-  helper_method :current_user, :signed_in?
+  helper_method :current_user, :signed_in?, :maps_key
 
   def current_user
     User.find_by(session_token: session[:session_token]) if session[:session_token]
@@ -31,6 +31,10 @@ class ApplicationController < ActionController::Base
 
   def require_signed_in
     redirect_to new_session_url unless signed_in?
+  end
+
+  def maps_key
+    Figaro.env.maps_key
   end
 
 end
