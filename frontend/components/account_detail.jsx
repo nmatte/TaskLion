f1b4ba1e06e9efcec1b8b1ac2b8db163b906a18d
Nathan Matteson <nwmatteson@gmail.com>
2016-03-07 09:11:09 -0800

    polish stuff

--- frontend/components/account_detail.jsx
+++ frontend/components/account_detail.jsx
@@ -1,10 +1,34 @@
-var React = require('react');
+var React = require('react'),
+    SessionStore = require('../stores/session'),
+    ApiUtil = require('../util/api_util');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      name: ""
+    };
+  },
+
+  getNameFromStore: function () {
+    return SessionStore.user().fname + " " + SessionStore.user().lname;
+  },
+
+  componentDidMount: function() {
+    this.sessListener = SessionStore.addListener(this._sessionListener);
+    ApiUtil.fetchCurrentUser();
+  },
+
+  _sessionListener: function () {
+    this.setState({name: this.getNameFromStore()});
+  },
+
+  componentWillUnmount: function() {
+    this.sessListener.remove();
+  },
   render: function () {
     return (
       <div>
-        accountDetail
+        {this.state.name}
       </div>
     );
   }
